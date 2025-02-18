d12e6d4811ca0cd4b696586ce7bea7bd79c8ce7f
ShamsadAlam <nobleperson7571@gmail.com>
2025-02-18 10:42:45 +0530

    Employee Dashboard Completed

--- src/components/Dashboard/EmployessDashboards.jsx
+++ src/components/Dashboard/EmployessDashboards.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Others/Header";
 import TaskListNumbers from "../Others/TaskListNumbers";
+import TaskList from "../Tasklist/TaskList";
 
 const EmployessDashboards = () => {
   return (
     <div className="p-10 bg-[#1c1c1c] h-screen w-screen">
       <Header />
       <TaskListNumbers />
+      <TaskList />
     </div>
   );
 };
